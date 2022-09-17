eb0ea0948283aab2dd9ea65741d63248042e765d
HarmonySpace <jchavaspace@gmail.com>
2022-09-17 10:03:27 -0600

    0.02 -> organization

--- packsh1/script_2.sh
+++ packsh1/script_2.sh
@@ -1,5 +1,5 @@
 
-
+#script 2
 line1=($(who | tr -s ' ' | cut -d" " -f1 | sort))
 
 k=($(echo ${#line1[*]}))
@@ -9,17 +9,7 @@ while [ "$i" -lt "$k" ]
 do
 	if [ "${line1[i-1]}" != ${line1[i]} ]
 	then
-		j=0
-		n=0
-		while [ "$j" -lt "$k" ]
-		do
-			if [ "${line1[i]}" == "${line1[j]}" ]
-			then
-				n=$((n+1))
-			fi
-			j=$((j+1))
-		done
-		echo "${line1[i]} $n"
+
 	fi
 	i=$((i+1))
 done

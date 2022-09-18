cddf0d91376007b87bbcd0d9a630a5f926ff4d23
HarmonySpace <jchavaspace@gmail.com>
2022-09-18 17:51:13 -0600

    0.05 -> add 2 first scripts

--- packsh2/script_2.sh
+++ packsh2/script_2.sh
@@ -17,16 +17,23 @@ echo "$log" | while read line
 do
     if [ -n "$line" ]
     then
+        k=1
+        while read -rn1 c
+        do
+            if [ -n "$c" ] && [ "$c" == ',' ]
+            then
+                k=$((k+1))
+            fi
+        done < <(echo "$line")
+        #imprime el nombre sel grupo
         echo "$log2" | while read line2
         do
             if [ "$j" == "$i" ]
             then
-                echo "$line2"
+                echo "$line2 $k"
             fi
             j=$((j+1))
         done
     fi
     i=$((i+1))
 done
-
-
